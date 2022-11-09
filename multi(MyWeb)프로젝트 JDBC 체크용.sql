1e3418aab61915a7a844df9f819720a1933925b7
INYONG KIM <ohnyong87@gmail.com>
2022-11-09 18:00:17 +0900

    Commit 10 MyWeb 프로젝트 multi/tiger 스키마 새로 작성한것에서 테스트 할것

---
+++ multi(MyWeb)프로젝트 JDBC 체크용.sql
@@ -0,0 +1,16 @@
+select * from member;
+
+desc member;
+
+--회원번호에 사용할 시퀀스 생성
+create sequence member_seq
+start with 1
+increment by 1
+nocache;
+
+alter table member add constraint member_userid_uk unique(userid);
+
+update member set status=-1 where idx=3;
+update member set status=-2 where idx=7;
+commit;
+alter table member rename column milage to mileage;
